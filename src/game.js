8a411226b69f59695151648e94bca0b5df41cd97
Eric Tran <eric.tran119@gmail.com>
2021-06-14 17:17:58 -0400

    collidng part done

--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@ const Asteroid = require("./asteroid.js");
 function Game(){
   this.DIM_X = 1920;
   this.DIM_Y = 1080;
-  this.NUM_ASTEROIDS = 25;
+  this.NUM_ASTEROIDS = 4;
 
   this.addAsteroids(this.NUM_ASTEROIDS);
 };
@@ -22,7 +22,7 @@ Game.prototype.addAsteroids = function(numAsteroids){
     options = {};
 
     options.pos = this.randomPosition();
-
+    options.game = this;
     this.asteroids.push(new Asteroid(options));
   }
 };
@@ -40,5 +40,47 @@ Game.prototype.moveObjects = function(){
     asteroid.move();
   });
 }
+
+Game.prototype.wrap = function(pos){
+  if (pos[0] >= this.DIM_X){
+    pos[0] = 0
+  } else if (pos[0] <= 0){
+    pos[0] = this.DIM_X
+  }
+
+  if (pos[1] >= this.DIM_Y) {
+    pos[1] = 0
+  } else if (pos[1] <= 0) {
+    pos[1] = this.DIM_Y
+  }
+
+  return pos
+}
+
+Game.prototype.checkCollisions = function(){
+  for ( let i = 0; i < this.asteroids.length - 1 ; i++){
+    for (let j = i+1 ; j < this.asteroids.length ; j++){
+      if (this.asteroids[i].isCollidedWith(this.asteroids[j])) {
+        this.asteroids[i].collideWith(this.asteroids[j]);
+      }
+    }
+  }
+}
+
+Game.prototype.step = function(){
+  this.moveObjects();
+  this.checkCollisions();
+
+}
+
+Game.prototype.remove = function(asteroid){
+  for (let i = 0; i < this.asteroids.length; i++){
+    if (this.asteroids[i] === asteroid){
+      this.asteroids.splice(i,1)
+      break;
+    }
+  }
+}
+
 
 module.exports = Game;
--- src/moving_object.js
+++ src/moving_object.js
@@ -3,6 +3,7 @@ function MovingObject(options){
   this.vel = options.vel;
   this.radius = options.radius;
   this.color = options.color;
+  this.game = options.game;
 }
 
 MovingObject.prototype.draw = function(ctx){
@@ -19,6 +20,24 @@ MovingObject.prototype.draw = function(ctx){
 
 MovingObject.prototype.move = function(){
   this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]];
+  this.pos = this.game.wrap(this.pos);
+}
+
+MovingObject.prototype.isCollidedWith = function(otherObject){
+  [x_1, y_1] = this.pos;
+  [x_2, y_2] = otherObject.pos;
+  let distance = Math.sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
+  let radiiSum = this.radius + otherObject.radius
+  return distance <= radiiSum
+}
+
+MovingObject.prototype.collideWith = function(otherObject){
+  this.game.remove(otherObject);
+  this.game.remove(this);
 }
 
 module.exports = MovingObject;
+
+
+// this is math, not JavaScript
+//Dist([x_1, y_1], [x_2, y_2]) = sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
